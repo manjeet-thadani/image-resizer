32bf00dd099b0f8eec570ce80582715b4334f9cd
Manjeet Thadani <manjeet.thadani@gmail.com>
2021-01-10 16:46:02 +0530

    Register routes in application

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import compression from "compression";
 import express, { Request, Response, NextFunction } from "express";
 import ApplicationError from "./errors/application-error";
+import routes from "./routes";
 import { config } from "./config/app.config";
 
 const app = express();
@@ -13,6 +14,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("port", config.get("app.port"));
+
+app.use(routes);
 
 app.use((err: ApplicationError, req: Request, res: Response, next: NextFunction) => {
   if (res.headersSent) {
