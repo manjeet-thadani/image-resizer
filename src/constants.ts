771e094bc37141c2fe8d6388cde2fbedeffca273
Manjeet Thadani <manjeet.thadani@gmail.com>
2021-01-10 16:09:13 +0530

    Added logger

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+interface Dimension {
+  width: number;
+  height: number;
+}
+
+export const DIMENSIONS: Dimension[] = [
+  { width: 32, height: 32 },
+  { width: 150, height: 150 },
+  { width: 450, height: 450 },
+  { width: 800, height: 800 },
+];
---
+++ src/logger.ts
@@ -0,0 +1,16 @@
+import { createLogger, format, transports } from "winston";
+
+const logTransports = [
+  new transports.Console({
+    level: "debug",
+    format: format.prettyPrint(),
+  }),
+];
+
+const logger = createLogger({
+  format: format.combine(format.timestamp()),
+  transports: logTransports,
+  defaultMeta: { service: "api" },
+});
+
+export default logger;
