eaffd3d3748c63b2fe566df76b1c65e4818b1ad9
Manjeet Thadani <manjeet.thadani@gmail.com>
2021-01-10 15:27:34 +0530

    Added custom request middleware

---
+++ src/middleware/request-middleware.ts
@@ -0,0 +1,44 @@
+import Joi from "@hapi/joi";
+import { RequestHandler, Request, Response, NextFunction } from "express";
+
+import logger from "../logger";
+import BadRequest from "../errors/bad-request";
+import { getMessageFromJoiError } from "../utils/joi";
+
+interface HandlerOptions {
+  validation?: Joi.ObjectSchema;
+}
+
+/**
+ * This router wrapper catches any error from async await
+ * and throws it to the default express error handler,
+ * instead of crashing the app
+ * @param handler Request handler to check for error
+ */
+const requestMiddleware = (handler: RequestHandler, options?: HandlerOptions): RequestHandler => async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // check if any validation schema was provided
+  // if yes then validate request against it
+  if (options?.validation) {
+    const { error } = options?.validation.validate(req, { allowUnknown: true });
+    if (error != null) {
+      return next(new BadRequest(getMessageFromJoiError(error)));
+    }
+  }
+
+  try {
+    await handler(req, res, next);
+  } catch (err) {
+    logger.log({
+      level: "error",
+      message: "Error in request handler",
+      error: err
+    });
+    return next(err);
+  }
+};
+
+export default requestMiddleware;
